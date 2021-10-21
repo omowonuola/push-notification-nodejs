0a685f283ab7126d07172ab6bf31ba5ad5048af6
omowonuola <oyedmola@gmail.com>
2021-10-21 02:52:21 +0100

    adds client folder setup for notification display

--- routes.js
+++ routes.js
@@ -4,13 +4,13 @@ import { WebPushError } from 'web-push';
 const router = express.Router();
 
 // Subscribe Route
-app.post('/subscribe', async(req, res) => {
+router.post('/subscribe', async(req, res) => {
     // Get pushSubscription object
     try {
-        const subscription = req.body;
+        const subscription = await req.body;
 
         // send 201 - resource created
-        await res.status(201).json({});
+         res.status(201).json({});
 
         // create payload
         const payload = JSON.stringify({ title: 'Push Test'})
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import webpush from 'web-push'
 import path from 'path'
+import topicRoutes from './routes.js'
 import colors from 'colors'
 
 
@@ -10,9 +11,15 @@ import colors from 'colors'
 dotenv.config()
 
 const app = express()
+
+const __dirname = path.resolve();
+// Set static path
+app.use(express.static(path.join(__dirname, 'client')));
 app.use(cors());
 app.use(express.urlencoded({limit: '50mb', extended: true }));
 app.use(express.json({limit: '50mb'}));
+
+app.use('/api', topicRoutes)
 
 const publicVapidKey = process.env.PUBLICVAPIDKEY
 const privateVapidKey = process.env.PRIVATEVAPIDKEY
