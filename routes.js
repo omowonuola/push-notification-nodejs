70f9e9019359d7274a26f5a045560d841f9e069a
omowonuola <oyedmola@gmail.com>
2021-10-23 18:40:29 +0100

    adds client file for notification

--- routes.js
+++ routes.js
@@ -1,34 +1,30 @@
 import express from 'express'
-import { WebPushError } from 'web-push';
+import webpush from 'web-push'
+
 
 const router = express.Router();
 
+
 // Subscribe Route
-router.post('/subscribe', async(req, res) => {
+router.post('/subscribe/topic', async(req, res) => {
     // Get pushSubscription object
     try {
         const subscription = await req.body;
-
+        console.log(subscription, 'jamz')
         // send 201 - resource created
          res.status(201).json({});
 
         // create payload
-        const payload = JSON.stringify({ title: 'Push Test'})
-
+        const payload = JSON.stringify({ title: 'create subscription' })
+        console.log(payload, 'lush')
         // Pass Object into sendNotification
-        WebPush.sendNotification(susbscription, payload)
+        webpush.sendNotification(subscription, payload)
 
     } catch (error) {
         console.log(error)
     }
     
 })
-
-
-
-
-
-
 
 
 
--- server.js
+++ server.js
@@ -19,14 +19,40 @@ app.use(cors());
 app.use(express.urlencoded({limit: '50mb', extended: true }));
 app.use(express.json({limit: '50mb'}));
 
-app.use('/api', topicRoutes)
+app.use('/', topicRoutes)
 
 const publicVapidKey = process.env.PUBLICVAPIDKEY
 const privateVapidKey = process.env.PRIVATEVAPIDKEY
 
-webpush.setVapidDetails('mailto:test@test.com', publicVapidKey, privateVapidKey);
+webpush.setVapidDetails(
+    'mailto:test@test.com',
+    publicVapidKey, 
+    privateVapidKey
+);
 
 
+
+// app.post('/subscribe/topic', (req, res) => {
+//     // Get pushSubscription object
+//     try {
+//         const subscription =  req.body;
+
+
+//         console.log(subscription, 'jamz')
+//         // send 201 - resource created
+//          res.status(201).json({});
+
+//         // create payload
+//         const payload = JSON.stringify({ topic: 'create subscrption' })
+//         console.log(payload, 'lush')
+//         // Pass Object into sendNotification
+//         webpush.sendNotification(subscription, payload)
+
+//     } catch (error) {
+//         console.log(error)
+//     }
+    
+// })
 
 const PORT = process.env.PORT || 8081
 
